1fa98545899ea9ca53945d5c28d04b1d8ad18669
mohammedaboualpha <mohammed.abou@gritacademy.se>
2025-05-03 12:42:51 +0200

    ...

---
+++ Js/script.js
@@ -0,0 +1,27 @@
+/*
+alert( "hej" )
+document.write( "<input style='background-color: green;'>" )
+console.log( "mohammed är 32 år gammal" )
+*/
+
+
+
+
+// 1) djikkass
+alert ('hejjjjjjjjjjsan')
+
+let box1
+box1 = 5
+box1 = 10
+box1 = 1 + 2
+box1 = "hej" + "san"
+box1 = (2+2)*3
+// p parentes
+// E xponent
+// M ultiplikation
+// D ivision
+// A ddiction
+// S ubtraktion
+box1 =  1 + 1 + "hej"
+
+console.log(box1)
